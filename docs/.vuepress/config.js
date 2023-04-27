31d66b63f77175a9f3d59c23259321a2f0f4bf2f
zhadongmin <1440423633@qq.com>
2023-04-27 23:13:44 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: "操作备忘录",
+    desciption: "能简单查到的东西不需要记忆",
+    author: "zhadongmin",
+    themeConfig: {
+        sidebar: [
+            {
+                title: '书签',
+                collapsable: false,
+                children: [
+                    {
+                        title: '网站',
+                        path: '/bookmark/website.md'
+                    },
+                    {
+                        title: '视频',
+                        path: '/bookmark/video.md'
+                    },
+                    {
+                        title: '开源',
+                        path: '/bookmark/opensoure.md'
+                    },
+                ]
+            },
+            {
+                title: 'TODO',
+                collapsable: true,
+                path: '/todo/todo.md',
+                children: [
+                ]
+            }
+        ]
+    }
+}
